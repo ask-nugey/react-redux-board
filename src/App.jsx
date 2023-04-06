85d511679f7e26251be12afc6358e2f525d96ca0
yuki-iwase <mischief.moi@gmail.com>
2023-04-07 04:25:57 +0900

    ✨ Add post article functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import "App.css";
-import { useSelector } from "react-redux";
+import { addPost } from "features/Posts";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [name, setName] = useState("")
+  const [content, setContent] = useState("")
   const postList = useSelector((state) => state.posts.value);
+
+  const dispatch = useDispatch()
+
+  const handleClick = () => {
+    dispatch(
+      addPost({
+        id: uuidv4(),
+        name: name,
+        content: content,
+      })
+    );
+  }
+
   return (
     <>
       <header>
         <h1>React-Redux 掲示板</h1>
       </header>
       <div className="c-addPost">
-        <input type="text" placeholder="お名前" />
-        <input type="text" placeholder="投稿内容" />
-        <button>投稿</button>
+        <input
+          type="text"
+          placeholder="お名前"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="投稿内容"
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button onClick={() => handleClick()}>投稿</button>
       </div>
       <main>
         <div className="c-postList">
