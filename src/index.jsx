23f67f0cd26b0216916ed4d9592b769e62e6135d
yuki-iwase <mischief.moi@gmail.com>
2023-04-07 03:28:21 +0900

    ✨ create postSlice

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import postReducer from './features/Posts';
 
 const store = configureStore({
   reducer: {
-    // #todo: これから作成
-    // posts: postsReducer,
+    posts: postReducer,
   },
 });
 
