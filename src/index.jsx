87a0f5d259f6ff503297835a3f5b8be8526e4576
yuki-iwase <mischief.moi@gmail.com>
2023-04-07 03:41:56 +0900

    ♻️ Convert relative paths to absolute paths

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'index.css';
-import App from './App';
+import App from 'App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import postReducer from 'features/Posts';
