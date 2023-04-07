221400231d3021c3c1faba7e4cba584b8198058d
yuki-iwase <mischief.moi@gmail.com>
2023-04-07 21:56:26 +0900

    デザインをよくする Fixes #1

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PostList from "component/PostList";
+import AddPost from "component/AddPost";
 
 const Main = () => {
   return (
-    <main>
+    <main className="c-main">
+      <AddPost />
       <PostList />
     </main>
   );
